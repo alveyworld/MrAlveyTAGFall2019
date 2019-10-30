659c63dc5450b14216b91ac5de5997611aa71fc9
20gutiewila <20gutiewila@washk12.org>
2019-10-30 11:04:47 -0600

    Update gutierreztag.py

--- gutierreztag.py
+++ gutierreztag.py
@@ -70,6 +70,17 @@ def render_introduction():
             "Mr. Alvey is sitting in his classroom and the\n" +
             "bell rings for first period.\n" + 
             "Ethan was here")
+
+def random_students():
+    '''
+    Return a random list of students, most sane, some insane
+    '''
+    
+def random_chillstaff():
+    '''
+    Return a random list of dq workers, most sane, some insane
+    '''
+
 def create_map():
     '''
     Creates a dictionary of the world map
@@ -77,6 +88,49 @@ def create_map():
     Returns:
         Map
     '''
+    return {
+        'classroom' : {
+            'about': 'You are at your desk in your classroom. The students are ready to learn.', 
+            'neighbors' : ['lounge', 'car'],
+            'stuff': ['ibuprofen', 'cliff bar', 'lotto ticket'],
+            'people': random_students()
+        },
+        'lounge' : {
+            'about': 'You sit in your regular spot and Hosner comments on your food.',
+            'neighbors' : ['classroom'],
+            'stuff': [],
+            'people': ['Kreitzer', 'Holt', 'Roberts', 'Hosner', 'Shaw', 'Dewitt', 'B']   
+        },
+        'car' : {
+            'about': 'You sit in your car and turn on the engine.',
+            'neighbors' : ['lins', 'home'],
+            'stuff': [],
+            'people': [],
+        },
+        'lins' : {
+            'about': 'You put on your apron and log into your register',
+            'neighbors' : ['car', 'dq'],
+            'stuff': [],
+            'people': ['Ashlee', 'Jeff', 'Collin'],    
+        },
+        'dq' : {
+            'about': 'You yell for help at the dq counter.',
+            'neighbors' : ['lins'],
+            'stuff': [],
+            'people': random_chillstaff(),    
+        },
+        'archam' : {
+            'neighbors' : ['home'],
+            'stuff': [],
+            'people': ['Joker', 'Counselor', 'Batman', 'Gordon', 'Dent', 'Alfred']    
+        },
+        'home' : {
+            'about': 'You made it home to your family. You are safe. You win',
+            'neighbors' : [],
+            'stuff': [],
+            'people': []
+        }
+    }
 
 def create_player():
     '''
@@ -86,34 +140,11 @@ def create_player():
         Player
     '''
     return {
-        'location': 'Classroom',
+        'location': 'classroom',
         'inventory': [],
         'hungry': False,
         'sanity': True,
         'money': 0
-        }
-
-def create_map():
-    '''
-    creates a dictionary of the world map
-    
-    Returns:
-        Map
-    '''
-    
-def create_player():
-    '''
-    Create a dictionary of the player
-    
-    Returns:
-        Player
-    '''
-    return {
-        'inventory': [],
-        'hungry': False,
-        'sanity': True,
-        'money': 0
-        }
         }
 
 def create_world():
@@ -123,19 +154,11 @@ def create_world():
     Returns:
         World: The initial state of the world
     '''
-<<<<<<< HEAD
-    def create_world():
-=======
->>>>>>> 467a56bc8c7522666cd95eef0d57d5d9fd4fd68b
     return {
         'map': create_map(),
         'player': create_player(),
         'status': "playing"
     }
-<<<<<<< HEAD
-    
-=======
->>>>>>> 467a56bc8c7522666cd95eef0d57d5d9fd4fd68b
 
 def render(world):
     '''
