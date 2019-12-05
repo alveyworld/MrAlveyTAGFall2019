161c615c6d86a9c18ff8f2e563511b7fcc10fd8b
Duffskul <57013980+Duffskul@users.noreply.github.com>
2019-12-05 10:37:33 -0700

    Update TAGShadenLawrence.py

--- TAGShadenLawrence.py
+++ TAGShadenLawrence.py
@@ -549,7 +549,7 @@ def map_item(current_location):
 def create_player():
     return {
         'location': 'field',
-        'inventory': ['map'],
+        'inventory': [],
         'Gold': 0
         }
 
