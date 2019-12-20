a75333bcebfba4663d5486fa472bde1b45a80346
someonestolemyusername <dallinjones@gmail.com>
2019-12-20 14:50:46 -0700

    Update JonesTAG.py

--- JonesTAG.py
+++ JonesTAG.py
@@ -13,9 +13,9 @@ Be sure to unit test and document your functions.
 ##### 1) Author Info #####
 
 # Change these three fields
-__author__ = "zack.alvey@washk12.org"
-__title__ = "Name of your game goes here"
-__description__ = "Replace this with a quick description of your game."
+__author__ = "20jonesdal@washk12.org"
+__title__ = "alvey's life"
+__description__ = "good luck."
 
 # Leave these two fields unchanged
 __version__ = 1
@@ -68,17 +68,15 @@ def render_introduction():
             "After getting to school Mr. Alvey must earn\n" +
             "enough money to get home without going insane.\n" +
             "Mr. Alvey is sitting in his classroom and the\n" +
-            "bell rings for first period.\n")
-
+            "bell rings for first period.\n" + 
+            "Ethan was here")
 def random_students():
     '''
-    Return a random list of students, most sane, some insane
-    '''
-    
+Return a random list of students, most sane, some insane
+'''
+
 def random_chillstaff():
-    '''
-    Return a random list of dq workers, most sane, some insane
-    '''
+    pass
 
 def create_map():
     '''
@@ -92,7 +90,7 @@ def create_map():
             'about': 'You are at your desk in your classroom. The students are ready to learn.', 
             'neighbors' : ['lounge', 'car'],
             'stuff': ['ibuprofen', 'cliff bar', 'lotto ticket'],
-            'people': random_students()
+            'people': random_people()
         },
         'lounge' : {
             'about': 'You sit in your regular spot and Hosner comments on your food.',
@@ -159,52 +157,6 @@ def create_world():
         'status': "playing"
     }
 
-def render_location(world):
-    '''
-    Consume a world and produce a string describing the location
-    '''
-    location = world['player']['location']
-    here = world['map'][location]
-    about = here['about']
-    
-    return ("You are in "+location+"\n"+
-            about+"\n")
-
-def render_player(world):
-    '''
-    Consume a world and produce a string describing the player
-    '''
-    
-    hungry = world['player']['hungry']
-    sanity = world['player']['sanity']
-    money = world['player']['money']
-    
-    statement = ""
-    if hungry:
-        statement += "You are hungry, you should eat soon. "
-    if not sanity:
-        statement += "You are going insane, please get help."
-    statement += "You have " + str(money) + " dollars."
-    
-    return statement
-    
-def render_visible_stuff(world):
-    '''
-     Consumes a world and produces a string of visible items
-     '''
-    location = world['player']['location']
-    here = world['map'][location]
-    stuff = here['stuff']
-    inventory = world['player']['inventory']
-    
-    if location == 'classroom':
-        return "You see a desk that might have something useful to you inside."
-    else:
-        visible_stuff = []
-        for thing in stuff:
-            visible_stuff.append(thing)
-        return "You see: " + ', '.join(visible_stuff)
-    
 def render(world):
     '''
     Consumes a world and produces a string that will describe the current state
@@ -216,9 +168,6 @@ def render(world):
     Returns:
         str: A textual description of the world.
     '''
-    return (render_location(world) +
-            render_player(world) +
-            render_visible_stuff(world))
 
 def get_options(world):
     '''
@@ -231,36 +180,7 @@ def get_options(world):
     Returns:
         list[str]: The list of commands that the user can choose from.
     '''
-    
-    commands = ["quit"]
-    current_location = world["player"]["location"]
-    location = world['map'][current_location]
-    neighbors = location['neighbors']
-    
-    for neighbor in neighbors:
-        commands.append("go to " + neighbor)
-    
-    if current_location == "classroom":
-        commands.append("search desk")
-        commands.append("teach")
-    if current_location == "lounge":
-        commands.append("complain")
-        commands.append("eat")
-    if current_location == "lins":
-        commands.append("ring up")
-    if current_location == "dq":
-        commands.append("eat")
-        commands.append("get icecream")
-    if current_location == "archam":
-        commands.append("counseling")
-        commands.append("meet joker")
-    return commands
-    
-def goto(world, command):
-    new_location = command[len('go to '):]
-    world['player']['location'] = new_location
-    return "You went to " + new_location
-    
+
 def update(world, command):
     '''
     Consumes a world and a command and updates the world according to the
@@ -273,19 +193,7 @@ def update(world, command):
     Returns:
         str: A message describing the change that occurred in the world.
     '''
-    if command == "quit":
-        world['status'] = 'quit'
-        return "You quit the game"
-    
-    if command.startswith('go to '):
-        return goto(world, command)
-    
-        
-    return "Unknown command: " + command
 
-def render_ending_lost(world):
-    return "You lost."
-    
 def render_ending(world):
     '''
     Create the message to be displayed at the end of your game.
@@ -296,12 +204,6 @@ def render_ending(world):
     Returns:
         str: The ending text of your game to be displayed.
     '''
-    if world['status'] == 'won':
-        return "You won!"
-    elif world['status'] == 'lost':
-        return render_ending_lost(world)
-    elif world['status'] == 'quit':
-        return "You quit."
 
 def choose(options):
     '''
@@ -318,14 +220,6 @@ def choose(options):
     Returns:
         str: The command that was selected by the user.
     '''
-    print("Available commands:")
-    for option in options:
-        print(option)
-        
-    command = input("Type a command: ")
-    while command not in options:
-        command = input("Invalid Command\n\nType a command: ")
-    return command
 
 ###### 4) Win/Lose Paths #####
 # The autograder will use these to try out your game
@@ -342,32 +236,6 @@ from cisc108 import assert_equal
 
 assert_equal("Mr. Alvey" in render_introduction(), True)
 assert_equal("classroom" in render_introduction(), True)
-
-player = create_player()
-# Use the built-in isinstance function to confirm that we made a dictionary
-assert_equal(isinstance(player, dict), True)
-# Does it have the right keys?
-assert_equal(len(player.keys()), 2)
-assert_equal("location" in player, True)
-assert_equal(player['location'], 'classroom')
-assert_equal("inventory" in player, True)
-assert_equal(player['inventory'], [])
-assert_equal("hungry" in player, True)
-assert_equal(player['hungry'], False)
-assert_equal("sanity" in player, True)
-assert_equal(player['sanity'], True)
-assert_equal("money" in player, True)
-assert_equal(player['money'], 0)
-
-new_map = create_map()
-
-assert_equal("classroom" in new_map, True)
-assert_equal("lounge" in new_map, True)
-assert_equal("car" in new_map, True)
-assert_equal("lins" in new_map, True)
-assert_equal("dq" in new_map, True)
-assert_equal("archam" in new_map, True)
-assert_equal("home" in new_map, True)
 
 
 ###### 6) Main Function #####
@@ -389,4 +257,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
